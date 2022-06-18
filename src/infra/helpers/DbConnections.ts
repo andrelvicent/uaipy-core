921b44734a199d429afce8ec867e70a6f0127204
André Luiz Vicente <andrevicente.ufu@gmail.com>
2022-06-18 12:32:41 -0300

    chore: add dialect options

--- src/infra/helpers/DbConnections.ts
+++ src/infra/helpers/DbConnections.ts
@@ -5,6 +5,9 @@ export const getPostgresConnection = () : Sequelize =>  {
   return new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    "dialectOptions": {
+      "ssl": true
+    },
     port: 5432
   });
 }
